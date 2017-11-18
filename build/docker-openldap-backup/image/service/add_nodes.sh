66abae833d399678f9349bf6c5b0d27256a68a93
Aleksei <ayra@1c.ru>
2017-11-18 17:21:56 +0300

    automate creation of ldap default nodes

---
+++ build/docker-openldap-backup/image/service/add_nodes.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+COMMAND=/container/service/add_nodes.sh
+DONEYET="${COMMAND}.alreadyrun"
+
+export PATH=/usr/bin:$PATH
+
+if [[ -f $DONEYET ]]; then
+  exit 1
+fi
+touch "$DONEYET"
+
+# Put the command you want to run exactly once here:
+ldapadd -x -D cn=admin,dc=base,dc=com -w adminpassword -f /container/service/slapd/assets/default_nodes/add_nodes.ldif
+
+
--- config/openldap/add_nodes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Adding default nodes..." 
-docker exec openldap ldapadd -x -D cn=admin,dc=base,dc=com -w adminpassword -f container/service/slapd/assets/default_nodes/add_nodes.ldif
-
