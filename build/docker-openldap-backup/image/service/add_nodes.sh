88c43e4bcd34eecdf873aeb0f683ca06d4f3ed40
Aleksei <ayra@1c.ru>
2017-11-21 14:54:44 +0300

    stop docker container in case of adding default nodes failure

--- build/docker-openldap-backup/image/service/add_nodes.sh
+++ build/docker-openldap-backup/image/service/add_nodes.sh
@@ -1,16 +1,7 @@
 #! /bin/bash
 
-COMMAND=/container/service/add_nodes.sh
-DONEYET="${COMMAND}.alreadyrun"
-
-export PATH=/usr/bin:$PATH
-
-if [[ -f $DONEYET ]]; then
-  exit 1
+ldapadd -x -D "cn=admin,$LDAP_BASE_DN" -w "$LDAP_ADMIN_PASSWORD" -f /container/service/slapd/assets/default_nodes/add_nodes.ldif
+code=$?
+if [[ "$code" != "0" && "$code" != "68" ]]; then
+  pkill -f /container/tool/run
 fi
-touch "$DONEYET"
-
-# Put the command you want to run exactly once here:
-
-ldapadd -x -D cn=admin,$LDAP_BASE_DN -w $LDAP_ADMIN_PASSWORD -f /container/service/slapd/assets/default_nodes/add_nodes.ldif
-
