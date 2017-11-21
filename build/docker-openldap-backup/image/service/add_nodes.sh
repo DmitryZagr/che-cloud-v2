b6ea4975d3403c596869a4b5d5eefab84380b980
Aleksei <ayra@1c.ru>
2017-11-21 11:19:51 +0300

    change algorithm of adding default ldap nodes by one that uses wait-for-it.sh

--- build/docker-openldap-backup/image/service/add_nodes.sh
+++ build/docker-openldap-backup/image/service/add_nodes.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-COMMAND=/container/service/add_nodes.sh
-DONEYET="${COMMAND}.alreadyrun"
+#COMMAND=/container/service/add_nodes.sh
+#DONEYET="${COMMAND}.alreadyrun"
 
-export PATH=/usr/bin:$PATH
+#export PATH=/usr/bin:$PATH
 
-if [[ -f $DONEYET ]]; then
-  exit 1
-fi
-touch "$DONEYET"
+#if [[ -f $DONEYET ]]; then
+#  exit 1
+#fi
+#touch "$DONEYET"
 
 # Put the command you want to run exactly once here:
-ldapadd -x -D cn=admin,dc=base,dc=com -w adminpassword -f /container/service/slapd/assets/default_nodes/add_nodes.ldif
 
+echo "Adding default nodes..."
+ldapadd -x -D cn=admin,dc=base,dc=com -w adminpassword -f /container/service/slapd/assets/default_nodes/add_nodes.ldif
 
